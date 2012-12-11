eac8605f7c73b850ab4987e92935a69812637aa2
Cloudfactory Workstation <cloudfactory@Cloudfactory-Workstations-iMac-3.local>
2012-12-11 17:32:17 +0545

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BestDeals::Application.config.secret_token = '274bda6b796f8d4c213023c3b5123e4262499c672664f48a1c805789b53c8d91ccc6c73a40dccb02344ceac25073a4fb1467b868b0884878c41a44b5528c3397'
